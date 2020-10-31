23ebe4b2eb6d53f2970c132f03bf98817e594a10
shaannessy25 <shaan.hurley25@gmail.com>
2020-10-30 21:07:45 -0700

    adding unit test for string library

--- index.js
+++ index.js
@@ -4,17 +4,50 @@ const capitalize = (str) => {
     return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase()
 }
 
-let str = 'foo bAr'
-console.log(capitalize(str))
-
 
 //Challenge 2 takes input string and capitalizes every letter of the string
 const allCaps = (str) => {
     return str.toUpperCase()
 }
 
-console.log(allCaps(str))
+
+//Challenge 3 take input string and capitalizes every first letter of every word
+const capitalizeWords = (str) => {
+    
+    return str.toLowerCase()
+}
+
+const removeExtraSpaces = (str) => {
+    return null
+}
+
+
+const kabobCase = (str) => {
+    return null
+}
+
+const snakeCase = (str) => {
+    return null
+}
+
+const camelCase = (str) => {
+    return null
+}
+
+
+const shift = (str) => {
+    return null
+}
+
+
+
 
 // export{capitalize, allCaps}
 module.exports.capitalize = capitalize
 module.exports.allCaps = allCaps
+module.exports.capitalizeWords = capitalizeWords
+module.exports.removeExtraSpaces = removeExtraSpaces
+module.exports.kabobCase = kabobCase
+module.exports.snakeCase = snakeCase
+module.exports.camelCase = camelCase
+module.exports.shift = shift
--- tests/test.js
+++ tests/test.js
@@ -2,11 +2,46 @@
 const stringLib = require('../index')
 
 
+
 test("Testing capitalize",  () => {
     expect(stringLib.capitalize("hello")).toBe("Hello")
+    expect(stringLib.capitalize("hELLO")).toBe("Hello")
 })
 
 
 test("Testing allCaps", () => {
     expect(stringLib.allCaps('hello')).toBe("HELLO")
+    expect(stringLib.allCaps('HeLlo')).toBe("HELLO")
+})
+
+test("Testing capitalize words", () => {
+    expect(stringLib.capitalizeWords('do all the things')).toBe('Do All The Things')
+    expect(stringLib.capitalizeWords('Do aLl tHe thIngs')).toBe('Do All The Things')
+})
+
+
+test("Testing removeExtraSpaces from input string", () =>  {
+    expect(stringLib.removeExtraSpaces(" Hello")).toBe("Hello")
+    expect(stringLib.removeExtraSpaces(" Hello World ! ")).toBe("Hello World!")
+})
+
+test("Testing kabob case", () => {
+    expect(stringLib.kabobCase("Hello World")).toBe("Hello-World")
+    expect(stringLib.kabobCase("find my folder")).toBe("find-my-folder")
+})
+
+test("Testing snakeCase", () => {
+    expect(stringLib.snakeCase("Hello World")).toBe("Hello_World")
+    expect(stringLib.snakeCase("find my folder")).toBe("find_my_folder")
+})
+
+test("Testing camelCase", () => {
+    expect(stringLib.camelCase("Hello World")).toBe("helloWorld")
+    expect(stringLib.camelCase("Correct path")).toBe("correctPath")
+
+})
+
+test("Testing Shift", () => {
+    expect(stringLib.shift("Hello")).toBe("elloH")
+    expect(stringLib.shift("foo bar", 3)).toBe("barfoo")
 })
